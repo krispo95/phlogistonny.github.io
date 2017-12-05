0a2ad17859c53427b72bf64a5f60849321df1a8d
Phlogistonny <pogosyan.chris@gmail.com>
2017-12-06 00:07:23 +0300

    Сделан шаблон сайта новостей

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+function nextPage(){
+    var title = document.getElementById("a-title");
+    var text = document.getElementById("a-text");
+    title.innerHTML = "Eger!";
+    text.innerHTML = "NextText"
+}
