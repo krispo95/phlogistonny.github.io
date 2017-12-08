e8efc6a3d42c0bd54612a03c07eeeb60bb88bd60
='phlogostonny <=>
2017-12-08 23:53:23 +0300

    switching articles

--- js/main.js
+++ js/main.js
@@ -1,8 +1,50 @@
-
+var title = document.getElementById("a-title");
+var text = document.getElementById("a-text");
+var img = document.getElementById("a-img");
+var i = 1;
+var arrObj = [
+    {
+        title: "First",
+        text: "First text",
+    },
+    {
+        title: "Second",
+        text: "Second text",
+    },
+    {
+        title: "Third",
+        text: "Third text",
+    },
+    {
+        title: "Hard",
+        text: "Bga text",
+    },
+];
+title.innerHTML = arrObj[i].title;
+text.innerHTML = arrObj[i].text;
 
 function nextPage(){
-    var title = document.getElementById("a-title");
-    var text = document.getElementById("a-text");
-    title.innerHTML = "Eger!";
-    text.innerHTML = "NextText"
+    i++;
+    if (i>arrObj.length - 1) {
+        //alert ("Это последняя статья!"); 
+        i = arrObj.length - 1;
+        return;
+        
+    }
+
+    title.innerHTML = arrObj[i].title;
+    text.innerHTML = arrObj[i].text;
+    //img.src = "img/223.jpg";
+   // console.log(img);
+}
+function prevPage(){
+    i--; 
+    if (i<0) {
+        //alert ("Это первая статья!"); 
+        i = 0;
+        return;
+        
+    }
+    title.innerHTML = arrObj[i].title;
+    text.innerHTML = arrObj[i].text;
 }
