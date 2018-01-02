01eac67415d6b9af6508ab924274fc723bad17e2
Phlogistonny <pogosyan.chris@gmail.com>
2018-01-03 00:40:35 +0300

    users can create new articles

--- js/main.js
+++ js/main.js
@@ -1,60 +1,65 @@
+var ARTICLES_KEY = "articles";
+
+var articles = localStorage.getItem(ARTICLES_KEY);
+if (articles == null) {
+    articles = [
+        {
+            title: "First",
+            text: "First text",
+        },
+    ];
+} else {
+    articles = JSON.parse(articles);
+}
+
 var title = document.getElementById("a-title");
 var text = document.getElementById("a-text");
 var img = document.getElementById("a-img");
-var popup = document.getElementById("a-popup")
-var i = 1;
-var arrObj = [
-    {
-        title: "First",
-        text: "First text",
-    },
-    {
-        title: "Second",
-        text: "Second text",
-    },
-    {
-        title: "Third",
-        text: "Third text",
-    },
-    {
-        title: "Hard",
-        text: "Bga text",
-    },
-];
-title.innerHTML = arrObj[i].title;
-text.innerHTML = arrObj[i].text;
+var popup = document.getElementById("a-popup");
+var inputArticle = document.getElementById("input-article");
+var inputTitle = document.getElementById("input-title");
 
-function nextPage(){
-    i++;
-    if (i>arrObj.length - 1) {
+var selectedArticle = 0;
+title.innerHTML = articles[selectedArticle].title;
+text.innerHTML = articles[selectedArticle].text;
+
+function nextPage() {
+    selectedArticle++;
+    if (selectedArticle > articles.length - 1) {
         //alert ("Это последняя статья!"); 
-        i = arrObj.length - 1;
+        selectedArticle = articles.length - 1;
         return;
-        
+
     }
 
-    title.innerHTML = arrObj[i].title;
-    text.innerHTML = arrObj[i].text;
+    title.innerHTML = articles[selectedArticle].title;
+    text.innerHTML = articles[selectedArticle].text;
     //img.src = "img/223.jpg";
-   // console.log(img);
+    // console.log(img);
 }
-function prevPage(){
-    i--; 
-    if (i<0) {
+function prevPage() {
+    selectedArticle--;
+    if (selectedArticle < 0) {
         //alert ("Это первая статья!"); 
-        i = 0;
+        selectedArticle = 0;
         return;
-        
+
     }
-    title.innerHTML = arrObj[i].title;
-    text.innerHTML = arrObj[i].text;
+    title.innerHTML = articles[selectedArticle].title;
+    text.innerHTML = articles[selectedArticle].text;
 }
-function showPopup(){
+function showPopup() {
     popup.style.visibility = "visible"
 }
-function closePopup(){
+function closePopup() {
     popup.style.visibility = "hidden"
 }
-function addArticle(){
-   
+function addArticle() {
+    articles.push({
+        title: inputTitle.value,
+        text: inputArticle.value,
+    });
+    localStorage.setItem(ARTICLES_KEY, JSON.stringify(articles));
+    closePopup();
+    selectedArticle = articles.length - 1;
 }
